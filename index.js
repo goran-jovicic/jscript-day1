434827d382c6fe0dcd490b18b7efc1646fc13af1
Mona Lisa <gugacns94@gmail.com>
2019-06-21 18:30:59 +0200

    asfasf

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 class PostOffice {
     constructor(){
         this.listaPisama = new Queue();
-
+    
         setInterval(async () => {
             try {
                 if (this.listaPisama.isEmpty()){
@@ -14,7 +14,7 @@ class PostOffice {
             }
         },10000);
     }
-
+    
     sendLetter = () =>
         new Promise((resolve,reject) =>
             setTimeout(() => {
@@ -23,72 +23,77 @@ class PostOffice {
                 :reject('Letter lost');
             },3000)
         );
-
-    createLetter = () => {
-  
-                setTimeout(() => {
-                    var per1 = new Customer('Pera','Peric');
-                    var per2 = new Customer('Marko','Markovic');
-                    var letter = (per1,per2,'test');
-                    this.listaPisama.enqueue(letter);
-                    // console.log('test');
-                    //resolve('Letter added');
-                    },3000);}
-
+    
+    createLetter = () => 
+        new Promise((resolve,reject)=>
+        setTimeout(() => {
+            var person1 = new Customer('Pera','Peric')
+            var person2 = new Customer('Pera','Peric')
+            var letter = (person1,person2,'test')
+            if(person1.firstName === person2.firstName && person1.lastName === person2.lastName){
+                reject('Ne moze ista osoba da salje i prima')
+            } else {
+                this.listaPisama.enqueue(letter);
+                resolve('Letter added');
+                // console.log('test');
+                //resolve('Letter added');
+            }
+        },3000)
+        );
     }
-
-class Person {
-    constructor(firstName,lastName){
-        if(this.constructor === Person){
-            throw new Error('abstract class')
+    
+    class Person {
+        constructor(firstName,lastName){
+            if(this.constructor === Person){
+                throw new Error('abstract class')
+            }
+            this.firstName = firstName;
+            this.lastName = lastName;
         }
-        this.firstName = firstName;
-        this.lastName = lastName;
     }
-}
-
-class Customer extends Person{
-    constructor(firstName,lastName){
-        super(firstName,lastName)
+    
+    class Customer extends Person{
+        constructor(firstName,lastName){
+            super(firstName,lastName)
+        }
+    
+        primiPismo = () => {
+            console.log('pismo');
+        }
     }
-
-    primiPismo = () => {
-        console.log('pismo');
+    
+    class Letter {
+        constructor(posiljalac,primaoc,sadrzaj){
+            this.posiljalac = posiljalac;
+            this.primaoc = primaoc;
+            this.sadrzaj = sadrzaj;
+        }
     }
-}
-
-class Letter {
-    constructor(posiljalac,primaoc,sadrzaj){
-        this.posiljalac = posiljalac;
-        this.primaoc = primaoc;
-        this.sadrzaj = sadrzaj;
-    }
-}
-
-class Queue {
-    //Constructor
-    constructor(){
-      //Private array for the queue
-      this.queue = [];
-    }
-    //At new item at the end of the queue
-    enqueue(item){
-      this.queue.push(item);
-    }
-    //Remove and return the item at the front of the queue
-    dequeue(){
-      return this.queue.shift();
-    }
-    //Returns if the queue is empty or not
-    isEmpty(){
-      return this.queue.length !== 0;
-    }
-   }
-
-// let customer = new Customer('Goran','Jovicic');
-// let customer2 = new Customer('Marko','Markovic');
-let postOffice = new PostOffice();
-postOffice.createLetter()
-// let pismo1 = new Letter(customer,customer2,'Tekst pisma');
-// console.log(postOffice.posaljiPismo(pismo1));
-// console.log(postOffice);
+    
+    class Queue {
+        //Constructor
+        constructor(){
+          //Private array for the queue
+          this.queue = [];
+        }
+        //At new item at the end of the queue
+        enqueue(item){
+          this.queue.push(item);
+        }
+        //Remove and return the item at the front of the queue
+        dequeue(){
+          return this.queue.shift();
+        }
+        //Returns if the queue is empty or not
+        isEmpty(){
+          return this.queue.length !== 0;
+        }
+       }
+    
+    // let customer = new Customer('Goran','Jovicic');
+    // let customer2 = new Customer('Marko','Markovic');
+    let postOffice = new PostOffice();
+    postOffice.createLetter()
+    // let pismo1 = new Letter(customer,customer2,'Tekst pisma');
+    // console.log(postOffice.posaljiPismo(pismo1));
+    // console.log(postOffice);
